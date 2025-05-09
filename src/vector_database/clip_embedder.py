65d6b58619bc811d2b466d05663754da966912bb
Sebastian Southwell <44193134+sebasouthwell@users.noreply.github.com>
2025-05-09 11:08:42 +0100

    feat: Added CLIP Embeddings

--- src/vector_database/clip_embedder.py
+++ src/vector_database/clip_embedder.py
@@ -1,7 +1,30 @@
+from numpy import ndarray
 from transformers import CLIPProcessor, CLIPModel
-
-class CLIPEmbedder:
+from typing import BinaryIO, Union
+from PIL import Image
+from src.vector_database.embedder import Embedder
+class CLIPEmbedder(Embedder):
     BASE_MODEL = "openai/clip-vit-base-patch32"
     PROCESSOR_MODEL = "openai/clip-vit-base-patch32"
 
     def __init__(self):
+        self.model = CLIPModel.from_pretrained(self.BASE_MODEL)
+        self.processor = CLIPProcessor.from_pretrained(self.PROCESSOR_MODEL)
+        self.chunk_size = 57
+    
+    def text_to_embedding(self,text : str) -> ndarray:
+        """Convert chunk data to embedding."""
+        inputs = self.processor(text=[text], return_tensors="pt", padding=True, truncation=True, max_length=self.chunk_size)
+        embedding = self.model.get_text_features(**inputs)
+        embedding = embedding[0] / embedding.norm()
+        embedding = embedding.numpy()
+        return embedding
+    
+    def image_to_embedding(self, image: Union[Image.Image, BinaryIO]) -> ndarray:
+        """Convert image data to embedding."""
+        inputs = self.processor(images=image, return_tensors="pt", padding=True, truncation=True, max_length=self.chunk_size)
+        embedding = self.model.get_image_features(**inputs)
+        embedding = embedding[0] / embedding.norm()
+        embedding = embedding.numpy()
+        return embedding
+    
--- src/vector_database/embedder.py
+++ src/vector_database/embedder.py
@@ -1,30 +1,27 @@
 from abc import ABC, abstractmethod
-
+from typing import BinaryIO, Union
+from PIL import Image
 from langchain_text_splitters import TokenTextSplitter
 from numpy import ndarray
 class Embedder(ABC):
     @abstractmethod
     def __init__(self):
+        self.chunk_size = 57
+        self.chunk_overlap = 20
         pass
 
-    @abstractmethod
-    def embed(self, text: str) -> list:
-        """Embed the given text into a vector representation."""
-        pass
-
-    def chunk_text(self,chunk_size=57, chunk_overlap=20):
+    def chunk_text(self):
         """Chunk the text into smaller pieces for embedding."""
-        text_splitter = TokenTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+        text_splitter = TokenTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap)
         chunks = text_splitter.split_text(self.text)
         pass
 
     @abstractmethod
-    def chunk_to_embedding(chunk_data : str) -> ndarray:
+    def text_to_embedding(chunk_data : str) -> ndarray:
         """Convert chunk data to embedding."""
         pass
 
-
     @abstractmethod
-    def embed_batch(self, texts: list) -> list:
-        """Embed a batch of texts into vector representations."""
+    def image_to_embedding(image: Union[Image.Image, BinaryIO]) -> ndarray:
+        """Convert image data to embedding."""
         pass
